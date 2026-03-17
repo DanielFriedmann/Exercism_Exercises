f8a08ed3a5b5bd23f7375111903634977b264133
dadanschel <d.friedmann14@gmail.com>
2026-03-17 11:17:30 +0100

    Binary Search

---
+++ csharp/Binary Search/Program.cs
@@ -0,0 +1,31 @@
+﻿public static class BinarySearch
+{
+    public static int Find(int[] input, int value)
+    {
+           Array.Sort(input);
+
+        int left = 0;
+        int right = input.Length - 1;
+
+        while (left <= right)
+        {
+            int middle = (left + right) / 2;
+
+            if (input[middle] == value)
+            {
+                return middle;
+            }
+            else if (value < input[middle])
+            {
+                right = middle - 1;
+            }
+            else
+            {
+                left = middle + 1;
+            }
+        }
+
+        return -1;
+    }
+    
+}
