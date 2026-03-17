b56a3211915d4b195b13d74c8eb2f8f5e66eb927
dadanschel <d.friedmann14@gmail.com>
2026-03-17 16:24:47 +0100

    Added Secret Handshake

---
+++ csharp/SecretHandshake/Program.cs
@@ -0,0 +1,26 @@
+﻿public static class SecretHandshake
+{
+    public static void Main(string[] args)
+    {
+        string[] tester = Commands(26);
+        foreach(string t in tester)
+        {
+            Console.WriteLine(t);
+        }
+    }
+    public static string[] Commands(int commandValue)
+    {
+        string binary = Convert.ToString(commandValue, 2);
+        binary = binary.PadLeft(5, '0');
+        char[] chars = binary.ToCharArray();
+        List<string> result = new List<string>();
+
+        if(chars[4] == '1') result.Add("wink");
+        if(chars[3] == '1') result.Add("double blink");
+        if(chars[2] == '1') result.Add("close your eyes");
+        if(chars[1] == '1') result.Add("jump");
+        if(chars[0] == '1') result.Reverse();
+
+        return result.ToArray();
+    }
+}
