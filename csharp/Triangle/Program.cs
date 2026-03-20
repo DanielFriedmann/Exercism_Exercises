0bb1e69b8fdde91e25c4de4b9cab670f91877a00
dadanschel <d.friedmann14@gmail.com>
2026-03-20 10:08:48 +0100

    Finished Triangle

--- csharp/Triangle/Program.cs
+++ csharp/Triangle/Program.cs
@@ -2,7 +2,7 @@
 {
     public static bool IsScalene(double side1, double side2, double side3)
     {
-        if(IsIsosceles(side1,side2,side3) || IsEquilateral(side1,side2,side3))
+        if((IsIsosceles(side1,side2,side3) || IsEquilateral(side1,side2,side3)) || !isTriangle(side1,side2,side3))
         {
             return false;
         }
@@ -11,7 +11,7 @@
 
     public static bool IsIsosceles(double side1, double side2, double side3) 
     {
-        if(side1 == side2 || side2 == side3 || side3 == side1)
+        if((side1 == side2 || side2 == side3 || side3 == side1) && isTriangle(side1,side2,side3))
         {
             return true;
         }
@@ -31,7 +31,12 @@
     {
         if(side1 > 0 && side2 > 0 && side3 > 0)
         {
-            return true;
+            if(side1 + side2 >= side3 && side2 + side3 >= side1 && side3 + side1 >= side2)
+            {
+                return true;
+            }
+            else return false;
+            
         }
         else return false;
     }
