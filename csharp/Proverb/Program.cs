0a3c18c11763e70e95a513435e2d759774c88d8f
dadanschel <d.friedmann14@gmail.com>
2026-03-23 09:27:00 +0100

    Added Proverb

---
+++ csharp/Proverb/Program.cs
@@ -0,0 +1,18 @@
+﻿public static class Proverb
+{
+    public static string[] Recite(string[] subjects)
+    {
+        string[] recites = new string[subjects.Length];
+        if (subjects.Length > 0)
+        {
+            for (int i = 0; i < subjects.Length - 1; i++)
+            {
+                recites[i] = $"For want of a {subjects[i].Trim()} the {subjects[i + 1].Trim()} was lost.";
+            }
+
+            recites[recites.Length - 1] = $"And all for the want of a {subjects[0].Trim()}.";
+        }
+
+        return recites;
+    }
+}
