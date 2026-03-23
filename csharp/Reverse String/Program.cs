21901f59232eebda3a396c7d2bd2d1c58d7923ad
dadanschel <d.friedmann14@gmail.com>
2026-03-23 09:07:55 +0100

    Reverse String added

---
+++ csharp/Reverse String/Program.cs
@@ -0,0 +1,14 @@
+﻿public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        char[] reverseA = new char[input.Length];
+        int counter = 0;
+        for (int i = input.Length; i > 0; i--)
+        {
+            reverseA[counter++] = input[i - 1];
+        }
+
+        return new string(reverseA);
+    }
+}
