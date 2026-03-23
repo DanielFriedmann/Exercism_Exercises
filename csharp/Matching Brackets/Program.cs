dfcff59cea7019b386d24335d37a6afc3a1fdb0b
dadanschel <d.friedmann14@gmail.com>
2026-03-23 13:25:54 +0100

    Added Matching brackets

---
+++ csharp/Matching Brackets/Program.cs
@@ -0,0 +1,31 @@
+﻿public static class MatchingBrackets
+{
+    public static bool IsPaired(string input)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char c in input)
+        {
+            if (c == '(' || c == '[' || c == '{')
+            {
+                stack.Push(c);
+            }
+            else if (c == ')' || c == ']' || c == '}')
+            {
+                if (stack.Count == 0)
+                    return false;
+
+                char open = stack.Pop();
+
+                if ((c == ')' && open != '(') ||
+                    (c == ']' && open != '[') ||
+                    (c == '}' && open != '{'))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
