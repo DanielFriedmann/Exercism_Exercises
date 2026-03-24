90f285eded35625b38fc01d7d8c6971ccce1d7b5
dadanschel <d.friedmann14@gmail.com>
2026-03-24 13:26:06 +0100

    Added Eliuds Eggs

---
+++ csharp/Eliuds Eggs/Program.cs
@@ -0,0 +1,13 @@
+﻿public static class EliudsEggs
+{
+    public static int EggCount(int encodedCount)
+    {
+        string binary = Convert.ToString(encodedCount, 2);
+        int eggs = 0;
+        foreach(char c in binary)
+        {
+            if (c == '1') eggs++;
+        }
+        return eggs;
+    }
+}
