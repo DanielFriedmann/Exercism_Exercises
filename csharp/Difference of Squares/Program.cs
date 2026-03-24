e4af2ad69f97f8fa639c662908e0cb891abe368d
dadanschel <d.friedmann14@gmail.com>
2026-03-24 13:15:18 +0100

    Added Difference of Squares

---
+++ csharp/Difference of Squares/Program.cs
@@ -0,0 +1,29 @@
+﻿public static class DifferenceOfSquares
+{
+    public static int CalculateSquareOfSum(int max)
+    {
+        int temp = 0;
+        for(int i = 0; i <= max; i++)
+        {
+            temp += i;
+        }
+
+        return temp * temp;
+    }
+
+    public static int CalculateSumOfSquares(int max)
+    {
+        int temp = 0;
+        for(int i = 1; i <= max; i++)
+        {
+            temp += i * i;
+        }
+
+        return temp;
+    }
+
+    public static int CalculateDifferenceOfSquares(int max)
+    {
+        return CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+    }
+}
