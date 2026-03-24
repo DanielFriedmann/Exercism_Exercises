6ab8aa6c2e56b7b047802b1061ea8e746f5967c1
dadanschel <d.friedmann14@gmail.com>
2026-03-24 10:25:44 +0100

    LineUp Added

---
+++ csharp/LineUp/Program.cs
@@ -0,0 +1,23 @@
+﻿public static class LineUp
+{
+    public static string Format(string name, int number)
+    {
+        string numbers = number.ToString();
+        char last = numbers[numbers.Length - 1];
+        string twonumbers = "";
+        if (numbers.Length > 1)
+        {
+            twonumbers = numbers.Substring(numbers.Length - 2, 2);
+        }
+
+
+        string suffix;
+
+        if (last == '1' && twonumbers != "11") suffix = "st";
+        else if (last == '2' && twonumbers != "12") suffix = "nd";
+        else if (last == '3' && twonumbers != "13") suffix = "rd";
+        else suffix = "th";
+
+        return $"{name}, you are the {number}{suffix} customer we serve today. Thank you!";
+    }
+}
