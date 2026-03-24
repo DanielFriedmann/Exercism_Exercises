2af7b104ec10a089323f5ed29f40d129c8c532c8
dadanschel <d.friedmann14@gmail.com>
2026-03-24 13:49:13 +0100

    Added ISBN Verifier

---
+++ csharp/ISBN verifier/Program.cs
@@ -0,0 +1,35 @@
+﻿public static class IsbnVerifier
+{
+    public static bool IsValid(string number)
+    {
+        string neu = number.Replace("-", "");
+        if (neu.Length == 10)
+        {
+            if (neu[neu.Length - 1] == 'X')
+            {
+                int count = 10;
+                int sum = 10;
+                for (int i = 0; i < 9; i++)
+                {
+                    sum += (neu[i] - '0')* count--;
+                }
+
+                if (sum % 11 == 0) return true;
+                else return false;
+            }
+            else
+            {
+                int count = 10;
+                int sum = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    sum += (neu[i] - '0') * count--;
+                }
+
+                if (sum % 11 == 0) return true;
+                else return false;
+            }
+        }
+        else return false;
+    }
+}
