d540ae22337be3ee5dd93950e810e2b2b8bdb50b
dadanschel <d.friedmann14@gmail.com>
2026-03-25 09:33:11 +0100

    Abbreviate

---
+++ csharp/Acronym/Program.cs
@@ -0,0 +1,24 @@
+﻿public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        string temp = phrase.Replace("-", " ");
+        string[] phrases = temp.Split(" ");
+        string abbreviate = "";
+
+        foreach (string p in phrases)
+        {
+            if (string.IsNullOrWhiteSpace(p)) continue;
+
+            foreach (char c in p)
+            {
+                if (char.IsLetter(c))
+                {
+                    abbreviate += c;
+                    break;
+                }
+            }
+        }
+        return abbreviate.ToUpper();
+    }
+}
