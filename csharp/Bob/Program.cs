d951a12b267dbdbf41be6a436e345f99bd9f5aab
dadanschel <d.friedmann14@gmail.com>
2026-03-25 09:57:01 +0100

    Bob

---
+++ csharp/Bob/Program.cs
@@ -0,0 +1,16 @@
+﻿public static class Bob
+{
+    public static string Response(string statement)
+    {
+        bool isYelling =
+            statement.Any(char.IsLetter) &&
+            statement.All(c => !char.IsLetter(c) || char.IsUpper(c));
+        string trimmed = statement.Trim();
+
+        if (string.IsNullOrWhiteSpace(trimmed)) return "Fine. Be that way!";
+        else if (trimmed[^1] == '?' && isYelling) return "Calm down, I know what I'm doing!";
+        else if (trimmed[^1] == '?') return "Sure.";
+        else if (isYelling) return "Whoa, chill out!";
+        else return "Whatever.";
+    }
+}
