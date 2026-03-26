3e1a450083c88cb28f6d65820801f7db60362b0d
dadanschel <d.friedmann14@gmail.com>
2026-03-26 11:10:20 +0100

    Added Prime Factors

---
+++ csharp/Prime Factors/Program.cs
@@ -0,0 +1,23 @@
+﻿public static class PrimeFactors
+{
+    public static long[] Factors(long number)
+    {
+        List<long> longs = new List<long>();
+        long temp = number;
+        do
+        {
+            for (int i = 2; i <= temp; i++)
+            {
+                if (temp % i == 0)
+                {
+                    temp = temp / i;
+                    longs.Add(i);
+                    break;
+                }              
+            }
+
+        } while (temp > 1);
+
+        return longs.ToArray();
+    }
+}
