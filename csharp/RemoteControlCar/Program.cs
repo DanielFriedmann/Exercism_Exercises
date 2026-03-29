31d2d14452e32465e774606afdf545fbae41bb1b
dadanschel <d.friedmann14@gmail.com>
2026-03-29 10:19:28 +0200

    Need for Speed

---
+++ csharp/RemoteControlCar/Program.cs
@@ -0,0 +1,56 @@
+﻿class RemoteControlCar
+{
+    private int speed;
+    private int batteryDrain;
+    private int distanceDriven;
+    private int batteryStatus;
+
+    public RemoteControlCar(int speed, int batteryDrain, int distanceDriven = 0, int batteryStatus = 100)
+    {
+        this.speed = speed;
+        this.batteryDrain = batteryDrain;
+        this.distanceDriven = distanceDriven;
+        this.batteryStatus = batteryStatus;
+    }
+
+    public bool BatteryDrained()
+    {
+        if (batteryStatus - batteryDrain >= 0) return false;
+        else return true;
+    }
+
+    public int DistanceDriven() => distanceDriven;   
+
+    public void Drive()
+    {
+        if (!BatteryDrained())
+        {
+            distanceDriven += speed;
+            batteryStatus -= batteryDrain;
+        }
+    }
+
+    public static RemoteControlCar Nitro() => new RemoteControlCar(50, 4);
+    
+}
+
+class RaceTrack
+{
+    private int distance;
+
+    public RaceTrack(int distance)
+    {
+        this.distance = distance;
+    }
+    public bool TryFinishTrack(RemoteControlCar car)
+    {
+        while(!car.BatteryDrained() && car.DistanceDriven() < distance)
+        {
+            car.Drive();
+            if(car.DistanceDriven() >= distance) return true;
+        }
+
+        return false;
+    }
+
+}
---
+++ csharp/SplitSecondStopwatch/Program.cs
@@ -0,0 +1,37 @@
+﻿public enum StopwatchState
+{
+    Ready,
+    Running,
+    Stopped
+}
+
+public class SplitSecondStopwatch(TimeProvider time)
+{    
+    public StopwatchState State { get; }
+    public TimeSpan CurrentLap { get; }
+    public TimeSpan Total { get; }
+    public IReadOnlyCollection<TimeSpan> PreviousLaps { get; }
+
+    public void Start()
+    {
+        if ( State == StopwatchState.Ready )
+        {
+            State = StopwatchState.Running;
+        }
+    }
+
+    public void Stop()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public void Reset()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public void Lap()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+}
