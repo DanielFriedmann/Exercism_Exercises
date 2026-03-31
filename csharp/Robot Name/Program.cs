5574f68364ff424d40f4fb8b889c13cbfc5adf16
dadanschel <d.friedmann14@gmail.com>
2026-03-31 10:26:18 +0200

    Robot Name

---
+++ csharp/Robot Name/Program.cs
@@ -0,0 +1,58 @@
+﻿public class Robot
+{
+    private static HashSet<string> usedNames = new HashSet<string>();
+    private static Random random = new Random();
+
+    private string name;
+
+    public string Name
+    {
+        get
+        {
+            if (name == null)
+            {
+                name = GenerateUniqueName();
+            }
+            return name;
+        }
+    }
+
+    public void Reset()
+    {
+        if (name != null)
+        {
+            usedNames.Remove(name);
+        }
+
+        name = null;
+    }
+
+    private string GenerateUniqueName()
+    {
+        string newName;
+
+        do
+        {
+            newName = GenerateRandomName();
+        }
+        while (usedNames.Contains(newName));
+
+        usedNames.Add(newName);
+        return newName;
+    }
+
+    private string GenerateRandomName()
+    {
+        char[] chars = new char[5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (i < 2)
+                chars[i] = (char)('A' + random.Next(0, 26));
+            else
+                chars[i] = (char)('0' + random.Next(0, 10));
+        }
+
+        return new string(chars);
+    }
+}
