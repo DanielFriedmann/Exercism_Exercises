17dffc4e2a62279082f440824a878111b681dc71
dadanschel <d.friedmann14@gmail.com>
2026-04-02 10:36:33 +0200

    High School Exercise

---
+++ csharp/High School Sweethearts/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+
+public static class HighSchoolSweethearts
+{
+    public static string DisplaySingleLine(string studentA, string studentB) => $"{studentA,29} ♡ {studentB,-29}";
+
+    public static string DisplayBanner(string studentA, string studentB)
+    {
+        string banner = $@"
+     ******       ******
+   **      **   **      **
+ **         ** **         **
+**            *            **
+**                         **
+**     {studentA} +  {studentB}    **
+ **                       **
+   **                   **
+     **               **
+       **           **
+         **       **
+           **   **
+             ***
+              *
+";
+        return banner;
+    }
+
+    public static string DisplayGermanExchangeStudents(string studentA
+        , string studentB, DateTime start, float hours)
+    {
+        CultureInfo culture = new CultureInfo("de-DE");
+
+        return string.Format(culture,
+        "{0} and {1} have been dating since {2:d} - that's {3:N2} hours",
+        studentA,
+        studentB,
+        start,
+        hours
+        );
+    }
+}
