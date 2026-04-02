d6338cc3d8ac6e4e33b5df2c3a16524f948a1391
dadanschel <d.friedmann14@gmail.com>
2026-04-02 11:05:56 +0200

    Allergies

---
+++ csharp/Allergies/Program.cs
@@ -0,0 +1,37 @@
+﻿public enum Allergen
+{
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128
+}
+
+public class Allergies
+{
+    private readonly int mask;
+    public Allergies(int mask)
+    {
+        this.mask = mask;
+    }
+
+    public bool IsAllergicTo(Allergen allergen)
+    {
+        return (mask & (int)allergen) != 0;
+    }
+
+    public Allergen[] List()
+    {
+        List<Allergen> result = new List<Allergen>();
+
+        foreach(Allergen allergen in Enum.GetValues<Allergen>())
+        {
+            if(IsAllergicTo(allergen)) result.Add(allergen);
+        }
+
+        return result.ToArray();
+    }
+}
