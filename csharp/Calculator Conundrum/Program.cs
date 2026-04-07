45d6dd9972a3b4d9f9f30d700a13519e253ea8b7
dadanschel <d.friedmann14@gmail.com>
2026-04-07 11:40:05 +0200

    Exception Handling

---
+++ csharp/Calculator Conundrum/Program.cs
@@ -0,0 +1,37 @@
+﻿public static class SimpleCalculator
+{
+    public static string Calculate(int operand1, int operand2, string? operation)
+    {
+
+        try
+        {
+            if (operation == "+")
+                return $"{operand1} + {operand2} = {operand1 + operand2}";
+
+            else if (operation == "*")
+                return $"{operand1} * {operand2} = {operand1 * operand2}";
+
+            else if (operation == "/" && operand2 != 0)
+                return $"{operand1} / {operand2} = {operand1 / operand2}";
+
+            else if (operation == "/")
+                throw new DivideByZeroException();
+
+            else if (operation == "")
+                throw new ArgumentException("Empty String.");
+
+            else if (operation == null)
+                throw new ArgumentNullException("Operation cant be null.");
+
+            else
+                throw new ArgumentOutOfRangeException("False Operation.");
+        }    
+
+        catch (DivideByZeroException)
+        {
+            return $"Division by zero is not allowed.";
+        }      
+               
+
+    }
+}
