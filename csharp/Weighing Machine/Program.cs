793265266ce4e00974f3afb1975a86cf0a0b2ac3
dadanschel <d.friedmann14@gmail.com>
2026-04-08 11:01:24 +0200

    Properties Exercise

---
+++ csharp/Weighing Machine/Program.cs
@@ -0,0 +1,38 @@
+﻿class WeighingMachine
+{
+
+    public WeighingMachine(int precision)
+    {
+        Precision = precision;
+    }
+    // TODO: define the 'Precision' property
+
+    public int Precision { get; }
+
+    // TODO: define the 'Weight' property
+    private double weight;
+    public double Weight
+    {
+        get => weight;
+        set
+        {
+            if (value < 0) throw new ArgumentOutOfRangeException();
+
+            weight = value;
+        }
+    }
+
+    // TODO: define the 'TareAdjustment' property
+    public double TareAdjustment { get; set; } = 5.0;
+
+    // TODO: define the 'DisplayWeight' property
+
+    public string DisplayWeight
+    {
+        get
+        {
+            double result = Weight - TareAdjustment;
+            return result.ToString($"F{Precision}") + " kg";
+        }
+    }
+}
