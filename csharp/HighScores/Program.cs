f50ed65dc36086a8c7cca98a43747804f7340c88
dadanschel <d.friedmann14@gmail.com>
2026-04-09 13:01:29 +0200

    HighScores

---
+++ csharp/HighScores/Program.cs
@@ -0,0 +1,22 @@
+﻿public class HighScores
+{
+    private List<int> scores;
+    public HighScores(List<int> list)
+    {
+        scores = list;
+    }
+
+    public List<int> Scores() => scores;  
+
+    public int Latest() => scores[^1];    
+
+    public int PersonalBest() => scores.Max();
+    
+
+    public List<int> PersonalTopThree()
+    {
+       return scores.OrderByDescending(x => x)
+                    .Take(3)
+                    .ToList();
+    }
+}
