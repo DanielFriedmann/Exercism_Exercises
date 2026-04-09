b27b31416b54b8fe2fea71dcca05125bd959905b
dadanschel <d.friedmann14@gmail.com>
2026-04-09 11:10:16 +0200

    Interfaces Exercise

---
+++ csharp/Remote Control Competition/Program.cs
@@ -0,0 +1,72 @@
+﻿// TODO implement the IRemoteControlCar interface
+
+public interface IRemoteControlCar
+{
+    public int DistanceTravelled { get; set; }
+
+    void Drive() { }
+}
+
+public class ProductionRemoteControlCar : IRemoteControlCar, IComparable<ProductionRemoteControlCar>
+{
+    public int CompareTo(ProductionRemoteControlCar other)
+    {
+        return NumberOfVictories.CompareTo(other.NumberOfVictories);
+    }
+    private int distanceTravelled;
+    public int DistanceTravelled
+    {
+        get => distanceTravelled;
+        private set => distanceTravelled = value;
+    }
+
+    int IRemoteControlCar.DistanceTravelled
+    {
+        get => distanceTravelled;
+        set => distanceTravelled = value;
+    }
+
+    public int NumberOfVictories { get; set; }
+
+    public void Drive()
+    {
+        DistanceTravelled += 10;
+    }
+}
+
+public class ExperimentalRemoteControlCar : IRemoteControlCar
+{
+    private int distanceTravelled;
+    public int DistanceTravelled
+    {
+        get => distanceTravelled;
+        private set => distanceTravelled = value;
+    }
+
+    int IRemoteControlCar.DistanceTravelled
+    {
+        get => distanceTravelled;
+        set => distanceTravelled = value;
+    }
+
+    public void Drive()
+    {
+        DistanceTravelled += 20;
+    }
+}
+
+public static class TestTrack
+{
+    public static void Race(IRemoteControlCar car)
+    {
+        car.Drive();
+    }
+
+    public static List<ProductionRemoteControlCar> GetRankedCars(ProductionRemoteControlCar prc1,
+        ProductionRemoteControlCar prc2)
+    {
+        var rankedCars = new List<ProductionRemoteControlCar> { prc1, prc2 };
+        rankedCars.Sort();
+        return rankedCars;
+    }
+}
