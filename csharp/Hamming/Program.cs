974f9f9588cf763107848fb3f186a8ea7088a258
dadanschel <d.friedmann14@gmail.com>
2026-04-09 13:12:17 +0200

    Hamming

---
+++ csharp/Hamming/Program.cs
@@ -0,0 +1,22 @@
+﻿public static class Hamming
+{
+    public static int Distance(string firstStrand, string secondStrand)
+    {
+        if (firstStrand.Length != secondStrand.Length) throw new ArgumentException("Strands must be same Length");
+
+        int distance = 0;
+        for (int i = 0; i < firstStrand.Length; i++)
+        {
+            if (firstStrand[i] != secondStrand[i]) distance++;
+        }
+        return distance;
+
+        /* LINQ methode:
+
+        return firstStrand
+            .Zip(secondStrand, (a,b) => a != b)
+            .Count(x => x);
+        */
+
+    }
+}
