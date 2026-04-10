b8be2f1c7ace85c9c38ba1d91c35754f1aa2951f
dadanschel <d.friedmann14@gmail.com>
2026-04-10 14:01:43 +0200

    Grades Exercise

---
+++ csharp/Grade School/Program.cs
@@ -0,0 +1,42 @@
+﻿public class GradeSchool
+{
+    public Dictionary<int, List<string>> grades = new Dictionary<int, List<string>>();
+    public HashSet<string> students = new HashSet<string>();
+    public bool Add(string student, int grade)
+    {
+        if (students.Contains(student)) return false;
+
+        students.Add(student);
+
+        if (grades.ContainsKey(grade))
+        {
+            grades[grade].Add(student);
+        }
+        else grades.Add(grade, new List<string> { student });
+
+        return true;
+    }
+
+    public IEnumerable<string> Roster()
+    {
+        var roster = new List<string>();
+
+        foreach (var grade in grades.Keys.OrderBy(g => g))
+        {
+            foreach (string s in grades[grade].OrderBy(name => name))
+            {
+                roster.Add(s);
+            }
+        }
+        return roster;
+    }
+
+    public IEnumerable<string> Grade(int grade)
+    {
+
+        if (!grades.ContainsKey(grade))
+            return new List<string>();
+
+        return grades[grade].OrderBy(name => name);
+    }
+}
