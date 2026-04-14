92d4603ddd948613daf5b8d5f7371a1c0b6c0f8f
dadanschel <d.friedmann14@gmail.com>
2026-04-14 10:39:35 +0200

    ulong exercise

---
+++ csharp/Grains/Program.cs
@@ -0,0 +1,28 @@
+﻿public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        if (n > 0 && n < 65)
+        {
+            ulong grains = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                grains *= 2;
+            }
+            return grains;
+        }
+        else throw new ArgumentOutOfRangeException();
+    }
+
+    public static ulong Total()
+    {
+        ulong grains = 1;
+        ulong total = 0;
+        for (int i = 1; i <= 64; i++)
+        {
+            total += grains;
+            grains *= 2;
+        }
+        return total;
+    }
+}
