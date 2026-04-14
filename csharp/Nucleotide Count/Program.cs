cef84a4c7232cb04515b41b8756ecf7673cc8397
dadanschel <d.friedmann14@gmail.com>
2026-04-14 10:24:16 +0200

    Dictionary Exercise

---
+++ csharp/Nucleotide Count/Program.cs
@@ -0,0 +1,28 @@
+﻿public static class NucleotideCount
+{
+    public static IDictionary<char, int> Count(string sequence)
+    {
+        int countA = 0; int countC = 0; int countG = 0; int countT = 0;
+
+        foreach (char c in sequence)
+        {
+            switch (c)
+            {
+                case 'A': countA++; break;
+                case 'C': countC++; break;
+                case 'G': countG++; break;
+                case 'T': countT++; break;
+                default: throw new ArgumentException("Invalid Operation - Wrong String.");
+            }
+        }
+
+        return new Dictionary<char, int>
+        {
+            { 'A', countA },
+            { 'C', countC },
+            { 'G', countG },
+            { 'T', countT },
+        };        
+
+    }
+}
