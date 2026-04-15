fe2018b5965646ee64863ece6b1f2615df97bfbd
dadanschel <d.friedmann14@gmail.com>
2026-04-15 10:56:44 +0200

    String Exercise

---
+++ csharp/Series/Program.cs
@@ -0,0 +1,21 @@
+﻿public static class Series
+{
+    public static string[] Slices(string numbers, int sliceLength)
+    {
+        List<string> slices = new List<string>();
+
+        if (sliceLength <= 0) throw new ArgumentException("Slice Length cant be 0 or negative");
+
+        if (numbers.Length >= sliceLength)
+        {
+            while (numbers.Length >= sliceLength)
+            {
+                string temp = numbers.Substring(0, sliceLength);
+                slices.Add(temp);
+                numbers = numbers.Remove(0, 1);
+            }
+            return slices.ToArray();
+        }
+        else throw new ArgumentException("Slice Length too big for this string.");        
+    }
+}
