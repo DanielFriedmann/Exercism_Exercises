6e7b59d01eaf1a2c8036d1e31c3aefae77cf658e
dadanschel <d.friedmann14@gmail.com>
2026-04-15 11:21:42 +0200

    Armstrong Numbers

---
+++ csharp/Armstrong Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        if( number >= 0)
+        {
+            int pow = number.ToString().Length;
+            int sum = 0;
+
+            foreach(char c in number.ToString())
+            {
+                int digit = c - '0';
+                sum += (int)Math.Pow(digit, pow);
+            }
+
+            return sum == number;
+        }
+        else throw new ArgumentOutOfRangeException("");
+    }
+}
