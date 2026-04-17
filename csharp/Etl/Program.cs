1aa598de2c2f8487e4e638f28d2a0fd15061f587
dadanschel <d.friedmann14@gmail.com>
2026-04-17 09:55:15 +0200

    Dictionary Exercise

---
+++ csharp/Etl/Program.cs
@@ -0,0 +1,16 @@
+﻿public static class Etl
+{
+    public static Dictionary<string, int> Transform(Dictionary<int, string[]> old)
+    {
+        Dictionary<string, int> newdict = new Dictionary<string,int>();
+
+        foreach (var kvp in old)
+        {
+            foreach (string s in kvp.Value)
+            {
+                newdict.Add(s.ToLower(), kvp.Key);
+            }
+        }
+        return newdict;
+    }
+}
