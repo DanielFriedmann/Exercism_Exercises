87d7dbef4c579ef9fd147b06666c4db773b8b184
dadanschel <d.friedmann14@gmail.com>
2026-04-20 13:57:26 +0200

    Number Enum Exercise

---
+++ csharp/Perfect Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        if (number < 1) throw new ArgumentOutOfRangeException();
+        
+        List<int> divisors = new List<int>();
+        int aliquot = 0;
+
+        for (int i = 1; i <= number / 2; i++)
+        {
+            if (number % i == 0)
+            {
+                divisors.Add(i);
+                aliquot += i;
+            }
+        }
+
+        if(number == aliquot) return Classification.Perfect;
+
+        if(number < aliquot) return Classification.Abundant;
+
+        return Classification.Deficient;
+
+    }
+}
