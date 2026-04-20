757111ab24a79fae8adf6590b5643bc40797f1bc
dadanschel <d.friedmann14@gmail.com>
2026-04-20 13:44:13 +0200

    Reference Out Exercise

---
+++ csharp/Building Telemetry/Program.cs
@@ -0,0 +1,72 @@
+﻿public class RemoteControlCar
+{
+    private int batteryPercentage = 100;
+    private int distanceDrivenInMeters = 0;
+    private string[] sponsors = new string[0];
+    private int latestSerialNum = 0;
+
+    public void Drive()
+    {
+        if (batteryPercentage > 0)
+        {
+            batteryPercentage -= 10;
+            distanceDrivenInMeters += 2;
+        }
+    }
+
+    public void SetSponsors(params string[] sponsors)
+    {
+        this.sponsors = sponsors;
+    }
+
+    public string DisplaySponsor(int sponsorNum) => sponsors[sponsorNum];
+
+    public bool GetTelemetryData(ref int serialNum,
+        out int batteryPercentage, out int distanceDrivenInMeters)
+    {
+        if (serialNum < latestSerialNum)
+        {
+            batteryPercentage = -1; distanceDrivenInMeters = -1;
+            serialNum = latestSerialNum;
+            return false;
+        }
+        else
+        {
+            latestSerialNum = serialNum;
+            batteryPercentage = this.batteryPercentage;
+            distanceDrivenInMeters = this.distanceDrivenInMeters;
+            return true;
+        }
+    }
+
+    public static RemoteControlCar Buy()
+    {
+        return new RemoteControlCar();
+    }
+}
+
+public class TelemetryClient
+{
+    private RemoteControlCar car;
+
+    public TelemetryClient(RemoteControlCar car)
+    {
+        this.car = car;
+    }
+
+    public string GetBatteryUsagePerMeter(int serialNum)
+    {
+        int battery;
+        int distance;
+        
+        bool success = car.GetTelemetryData(ref serialNum, out battery, out distance);
+
+        if (!success || distance == 0) return "no data";
+        
+        else
+        {
+            int usage = (100 - battery) / distance;
+            return $"usage-per-meter={usage}";
+        }
+    }
+}
