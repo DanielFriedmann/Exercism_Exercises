4d25eecbef3628154f4bba88067b4a288b7256fa
dadanschel <d.friedmann14@gmail.com>
2026-04-21 14:57:18 +0200

    RNA Exercise

---
+++ csharp/RNA Translation/Program.cs
@@ -0,0 +1,29 @@
+﻿public static class RnaTranscription
+{
+
+    /* Given a DNA strand, its transcribed RNA strand is formed by replacing each nucleotide with its complement:
+
+        G -> C
+        C -> G
+        T -> A
+        A -> U
+    */
+
+    public static string ToRna(string strand)
+    {
+        string result = "";
+
+        foreach (char c in strand)
+        {
+            switch (c)
+            {
+                case 'G': result += "C"; break;
+                case 'C': result += "G"; break;
+                case 'T': result += "A"; break;
+                case 'A': result += "U"; break;
+                default: throw new ArgumentException("");
+            }
+        }
+        return result;
+    }
+}
