1c709bf2bb18df4c753f4a0d0e43d5b6dc3f72f7
dadanschel <d.friedmann14@gmail.com>
2026-04-24 10:05:00 +0200

    Arithmetic Overflow Exercise

---
+++ csharp/Hyperinflation Overflow/Program.cs
@@ -0,0 +1,34 @@
+﻿public static class CentralBank
+{
+    public static string DisplayDenomination(long @base, long multiplier)
+    {
+        try
+        {
+            return $"{checked(@base * multiplier)}";
+        }
+
+        catch (OverflowException)
+        {
+            return "*** Too Big ***";
+        }
+    }
+
+    public static string DisplayGDP(float @base, float multiplier)
+    {
+        if (!float.IsInfinity(@base * multiplier)) return $"{@base * multiplier}";
+        else return "*** Too Big ***";
+    }
+
+    public static string DisplayChiefEconomistSalary(decimal salaryBase, decimal multiplier)
+    {
+        try
+        {
+            return $"{salaryBase * multiplier}";
+        }
+
+        catch (OverflowException)
+        {
+            return "*** Much Too Big ***";
+        }
+    }
+}
