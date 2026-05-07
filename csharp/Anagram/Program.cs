987d18e9d86b1b3e0627abb3ef1e469d8c45d0c9
dadanschel <d.friedmann14@gmail.com>
2026-05-07 11:15:27 +0200

    LINQ Exercise

---
+++ csharp/Anagram/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Anagram
+{
+    private string BaseWord;
+    public Anagram(string baseWord)
+    {
+        BaseWord = baseWord;
+    }
+
+    public bool IsAnagram(string candidate)
+    {
+        if(BaseWord.Length != candidate.Length || BaseWord.ToLower() == candidate.ToLower()) return false;
+
+        char[] sortedBase = BaseWord.ToLower().OrderBy(c => c).ToArray();
+        char[] sortedCandidate = candidate.ToLower().OrderBy(c => c).ToArray();
+
+        return sortedBase.SequenceEqual(sortedCandidate);        
+
+    }
+
+    public string[] FindAnagrams(string[] potentialMatches)
+    {
+        return potentialMatches.Where(match => IsAnagram(match)).ToArray();
+    }
+}
