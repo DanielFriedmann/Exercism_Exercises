dba3ead7082f8b43040dbb90efc0a7300dfead08
dadanschel <d.friedmann14@gmail.com>
2026-05-12 14:27:41 +0200

    IDisposable and  using Exercise

---
+++ csharp/Orm in One Go/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+public class Orm : IDisposable
+{
+    private Database database;
+
+    public Orm(Database database)
+    {
+        this.database = database;
+    }
+
+    public void Write(string data)
+    {
+        using (database)
+        {
+            database.BeginTransaction();
+            database.Write(data);
+            database.EndTransaction();
+        }
+    }
+
+    public bool WriteSafely(string data)
+    {
+        try
+        {
+            using (database)
+            {
+                database.BeginTransaction();
+                database.Write(data);
+                database.EndTransaction();
+            }
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public void Dispose()
+    {
+        database.Dispose();
+    }
+}
