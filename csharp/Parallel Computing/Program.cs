f49c70ccebb12a716e4c8fe7ce943af511ad4e3e
dadanschel <d.friedmann14@gmail.com>
2026-05-20 10:30:29 +0200

    Task.Run and PLINQ Exercise

---
+++ csharp/Parallel Computing/Program.cs
@@ -0,0 +1,12 @@
+﻿public static class ParallelLetterFrequency
+{
+    public static Task<Dictionary<char, int>> Calculate(IEnumerable<string> texts)
+    {
+        return Task.Run(() => texts.AsParallel()
+                    .SelectMany(text => text)
+                    .Where(c => char.IsLetter(c))
+                    .Select(c => char.ToLower(c))
+                    .GroupBy(c => c)
+                    .ToDictionary(group => group.Key, group => group.Count()));
+    }
+}
