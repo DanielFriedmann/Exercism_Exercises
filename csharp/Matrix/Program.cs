8343c089ebf0b0079c57de6f0d9fac5a8cd620fe
dadanschel <d.friedmann14@gmail.com>
2026-05-21 10:09:49 +0200

    Grid and LINQ Exercise

---
+++ csharp/Matrix/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Matrix
+{
+    public int[][] Grid;
+
+    public Matrix(string input)
+    {
+        Grid = input.Split('\n')
+                    .Select(zeile => zeile.Split(' ').Select(int.Parse).ToArray())                    
+                    .ToArray();
+    }
+
+    public int[] Row(int row)
+    {        
+        if(row > Grid.Length || row <= 0) throw new ArgumentException("");
+
+        return Grid[row - 1];
+    }
+
+    public int[] Column(int col)
+    {
+        if(col > Grid[0].Length || col <= 0) throw new ArgumentException("");
+
+        return Grid.Select(zeile => zeile[col - 1])
+                   .ToArray();
+    }
+}
