eb710477c369efec488d9dd15836b93df9d2f42f
dadanschel <d.friedmann14@gmail.com>
2026-05-21 11:37:28 +0200

    LINQ and Regex Exercise

---
+++ csharp/Word Count/Program.cs
@@ -0,0 +1,12 @@
+﻿public static class WordCount
+{
+    public static IDictionary<string, int> CountWords(string phrase)
+    {
+        return phrase.Split(new char[] {' ', ',', '.', '!', '?', ':', '\n', '\t'}, StringSplitOptions.RemoveEmptyEntries)
+                     .Select(word => new string(word.Where(c => char.IsLetterOrDigit(c) || c == '\'').ToArray()))
+                     .Select(word => word.Trim('\'').ToLower())
+                     .Where(word => word.Length > 0)                     
+                     .GroupBy(word => word)
+                     .ToDictionary(word => word.Key, word => word.Count());
+    }
+}
